6093a5d1a2f26935aa6e5fbab943d825def26805
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2024-10-01 23:25:56 -0700

    format on save

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
-    secret: 'my-secret-key',
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false }
-}));
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // last middleware returns 404
 app.use((req, res, next) => {
-    res.status(404).send('Not Found!');
+  res.status(404).send("Not Found!");
 });
 
 // error handling middleware
 app.use((err, req, res, next) => {
-    res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
