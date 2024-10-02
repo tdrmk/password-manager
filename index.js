0c66dcda2aea25e677126072b27c6147ef789fc6
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2024-10-01 22:55:01 -0700

    static files

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
