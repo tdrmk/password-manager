8690fb486e1672206e3a7576dbbf2757bfafdd54
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2024-10-01 22:55:26 -0700

    404 + 500

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// last middleware returns 404
+app.use((req, res, next) => {
+    res.status(404).send('Not Found!');
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    res.status(500).send('Something went wrong!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
