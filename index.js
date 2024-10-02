93345873917f96d43dbbe4e387704e4c52c71c30
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2024-10-01 22:56:16 -0700

    express session, nodemon, dotenv init

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'my-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
