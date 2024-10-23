8406748d8e8e9c32b905947ad9830134c5dac88c
Dinesh Ram Kumar <tdineshramkumar@gmail.com>
2024-10-22 23:59:22 -0700

    password  validation improved

--- validate-utils.js
+++ validate-utils.js
@@ -12,8 +12,34 @@ export function validateUsername(username) {
 }
 
 export function validatePassword(password) {
-  if (password.length < 3) {
-    return "Password must be at least 3 characters long";
+  // password must be between 8 and 20 characters long
+  // password must contain at least one lowercase letter
+  // password must contain at least one uppercase letter
+  // password must contain at least one number
+  // password must contain at least one special character !@#$%^&*()-_+=
+  // password must only contain the above characters
+
+  if (password.length < 8) {
+    return "Password must be at least 8 characters long";
+  }
+  if (password.length > 20) {
+    return "Password must be at most 20 characters long";
+  }
+  if (!/[a-z]/.test(password)) {
+    return "Password must contain at least one lowercase letter";
+  }
+  if (!/[A-Z]/.test(password)) {
+    return "Password must contain at least one uppercase letter";
+  }
+  if (!/[0-9]/.test(password)) {
+    return "Password must contain at least one number";
+  }
+  if (!/[!@#$%^&*()-_+=]/.test(password)) {
+    return "Password must contain at least one special character among !@#$%^&*()-_+=";
+  }
+
+  if (!/^[a-zA-Z0-9!@#$%^&*()-_+=]+$/.test(password)) {
+    return "Password can only contain letters , numbers, and special characters among !@#$%^&*()-_+=";
   }
   return true;
 }
